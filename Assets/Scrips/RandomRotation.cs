4655a752d1720efcd3d895027fed8508e3ab136e
binins <yuebinbin@adesk.com>
2016-11-10 23:44:33 +0800

    三个岩石预制体的创建，可被子弹消灭

---
+++ Assets/Scrips/RandomRotation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotation : MonoBehaviour {
+
+
+	public float turnSpeed;
+	// Use this for initialization
+	void Start () {
+	
+		GetComponent<Rigidbody> ().angularVelocity = Random.insideUnitSphere * turnSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
